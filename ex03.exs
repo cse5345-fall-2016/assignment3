35f2f25b645a56587d8b3c954c2ca1e365698332
Jospaul Mahajan Prakash <iamjospaul@gmail.com>
2016-10-08 18:08:42 -0500

    ex03 working, but time assertion fails as the single thread is faster than 2 threads

--- ex03.exs
+++ ex03.exs
@@ -44,7 +44,7 @@ defmodule Ex03 do
         5	does it produce the correct results on any valid data
 
       Tested
-      if tests are provided as part of the assignment: 	
+      if tests are provided as part of the assignment:
         5	all pass
 
       Aesthetics
@@ -60,7 +60,19 @@ defmodule Ex03 do
   """
 
   def pmap(collection, process_count, function) do
-    « your code here »
+    me = self
+    collection
+      |>Enum.chunk(div(Enum.count(collection),process_count),
+        div(Enum.count(collection),process_count),[])
+      |>Enum.map(fn(elem) ->
+        spawn_link fn ->
+          send me, {self, elem } end end)
+      |> Enum.map(fn (pid) ->
+        receive do
+          { ^pid, elem } ->
+            Enum.map(elem, function) end
+        end)
+      |> Enum.concat
   end
 
 end
@@ -96,5 +108,5 @@ defmodule TestEx03 do
     assert result2 == result1
     assert time2 < time1 * 0.8
   end
-  
+
 end
