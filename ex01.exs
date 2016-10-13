8ad19ebb16931418ed5bb9da8a54972c53cfb91c
dgr8vj@gmail.com <viveksjain6@gmail.com>
2016-10-12 20:45:39 -0500

    ex01.exs |Fixed Type |>  Passed Test 1.

--- ex01.exs
+++ ex01.exs
@@ -29,7 +29,7 @@ defmodule Ex01 do
 
   def counter(value \\ 0) do
     receive do
-      {:next, from} -> send from {:next_is, value}
+      { :next, from } -> send from, {:next_is, value}
       counter(value+1)
     end
   end
@@ -45,20 +45,20 @@ defmodule Test do
   # This test assumes you have a function `counter` that can be spawned
   # and which handles the `{:next, from}` message
 
-  # test "basic message interface" do
-  #   count = spawn Ex01, :counter, []
-  #   send count, { :next, self }
-  #   receive do
-  #     { :next_is, value } ->
-  #       assert value == 0
-  #   end
-  #
-  #   send count, { :next, self }
-  #   receive do
-  #     { :next_is, value } ->
-  #       assert value == 1
-  #   end
-  # end
+  test "basic message interface" do
+    count = spawn Ex01, :counter, []
+    send count, { :next, self }
+    receive do
+      { :next_is, value } ->
+        assert value == 0
+    end
+
+    send count, { :next, self }
+    receive do
+      { :next_is, value } ->
+        assert value == 1
+    end
+  end
 
   # then uncomment this one
   # Now we add two new functions to Ex01 that wrap the use of
