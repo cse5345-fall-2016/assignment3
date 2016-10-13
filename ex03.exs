327716ec5eaf306e141ae829e9f4ef156b6cdaf5
dgr8vj@gmail.com <viveksjain6@gmail.com>
2016-10-12 23:36:59 -0500

    ex03.exs | Done

--- ex03.exs
+++ ex03.exs
@@ -1,3 +1,6 @@
+#Finished in 1.2 seconds (0.04s on load, 1.2s on tests) | MacBook Pro | 16 GB 1600 MHz DDR3 | 2.3 GHz Intel Core i7
+#1 out of 4 trials, the "pmap actually reduces time" test fails.
+
 defmodule Ex03 do
 
   @moduledoc """
@@ -44,7 +47,7 @@ defmodule Ex03 do
         5	does it produce the correct results on any valid data
 
       Tested
-      if tests are provided as part of the assignment: 	
+      if tests are provided as part of the assignment:
         5	all pass
 
       Aesthetics
@@ -59,9 +62,36 @@ defmodule Ex03 do
 
   """
 
+#Sources Used for pmap mentioned below:
+#http://stackoverflow.com/questions/32589216/task-async-in-elixir-stream
+# big_list
+# |> Stream.map(&Task.async(Module, :do_something, [&1]))
+# |> Stream.map(&Task.await(&1))
+# |> Enum.filter filter_fun
+
+
+#http://michal.muskala.eu/2015/08/06/parallel-downloads-in-elixir.html
+# defp fetch_and_save_batch(committers) do
+#   committers
+#   |> Enum.map(&Task.async(fn -> fetch_and_save(&1) end))
+#   |> Enum.map(&Task.await(&1, 10000))
+# end
+
+#http://milmazz.uno/article/2016/09/03/asynchronous-tasks-with-elixir/
+# Enum.map(&Task.async(fn ->
+#        generate_module_page(&1, output)
+#      end))
+#   |> Enum.map(&Task.await/1)
+
+
   def pmap(collection, process_count, function) do
-    « your code here »
+    size_of_chunk = round(Enum.count(collection)/process_count)
+    Enum.chunk(collection, size_of_chunk, size_of_chunk, [])
+    |>  Enum.map(&(Task.async(fn -> Enum.map(&1, function) end)))
+    |>  Enum.map(&(Task.await/1))
+    |> Enum.concat
   end
+
 
 end
 
@@ -96,5 +126,5 @@ defmodule TestEx03 do
     assert result2 == result1
     assert time2 < time1 * 0.8
   end
-  
+
 end
